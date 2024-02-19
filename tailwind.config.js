503f3fa433e0014565dfd1b4c41e984c2f351664
Shah Sohan <shahmdsohan.cse@gmail.com>
2024-02-19 13:07:53 +0600

    added nav and banner section

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+// image
+  module.exports = {
+    theme: {
+      backgroundSize: {
+        'auto': 'auto',
+        'cover': 'cover',
+        'contain': 'contain',
+        '50%': '50%',
+        '16': '4rem',
+      }
+    }
+  }
